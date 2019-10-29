0275c4a6e05a575b150721da55fd69ca373a33f2
Raul Cruz <raulcrbe@gmail.com>
2019-10-29 16:06:45 +0100

    Examen 1 profundizacion 2 push

--- src/test/java/es/upm/grise/profundizacion2019/ex1/ClaseExamenTest.java
+++ src/test/java/es/upm/grise/profundizacion2019/ex1/ClaseExamenTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 
 public class ClaseExamenTest {
     //recorre el bucle hasta que es igual, se mete en el if y da que result=0
+	// Se queda en bucle en el nodo 1 haciendo la resta, luego pasa al nodo 2 y entra por el if al nodo 3
+	//despues del if va al return que es el nodo 5
 	@Test
 	public void test() {
 		ClaseExamen clase = new ClaseExamen();
