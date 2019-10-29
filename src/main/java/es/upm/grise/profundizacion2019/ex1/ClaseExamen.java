e427e113e850f0cf446845cc679db37d0ebf5d8c
Raul Cruz <raulcrbe@gmail.com>
2019-10-29 16:04:09 +0100

    Examen 1 profundizacion

--- src/main/java/es/upm/grise/profundizacion2019/ex1/ClaseExamen.java
+++ src/main/java/es/upm/grise/profundizacion2019/ex1/ClaseExamen.java
@@ -5,18 +5,23 @@ public class ClaseExamen {
 	public float metodoAProbar(int a, int b) {
 		
 		float result = 0;
-		
+		//mientras a > b le resta uno hasta que sea igual
+		//Nodo 1
 		while( a > b) {
 			a--;
 		}
-		
+		//si a <=b entonces result =0
+		//Nodo 2
 		if (a <= b) {
+			//Nodo 3
 			result = 0;
 		}
+		//si no devuelve b, no se va a meter nunca aqui
 		else {
+			//Nodo 4
 			result = b;
 		}
-		
+		//Nodo 5
 		return result;
 	}
 
--- src/test/java/es/upm/grise/profundizacion2019/ex1/ClaseExamenTest.java
+++ src/test/java/es/upm/grise/profundizacion2019/ex1/ClaseExamenTest.java
@@ -5,10 +5,13 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ClaseExamenTest {
-
+    //recorre el bucle hasta que es igual, se mete en el if y da que result=0
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		ClaseExamen clase = new ClaseExamen();
+		assertEquals(0, clase.metodoAProbar(9, 6),0);
 	}
+	//NUNCA se va apoder meter en el else debido a que si A es mayor que B va a restar 1 a A hasta que se igualen,
+	// por lo que A siempre va a ser <= que B
 
 }
